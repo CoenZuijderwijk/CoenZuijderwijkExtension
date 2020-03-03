f37df989370d1ec383f074004bf475055dc5393d
CoenZuijderwijk <302323979@student.rocmondriaan.nl>
2020-03-03 16:34:23 +0100

    changes13

--- src/Niffo.php
+++ src/Niffo.php
@@ -1,8 +1,9 @@
 <?php
-   namespace CoenVlog;
-   class Niffos {
-      public static function world() {
-         return 'Ewa a niffos';
-      }
-   }
+namespace coen;
+class Niffos {
+    public $getal = 12;
+    public static function world() {
+        return 'Ewa a niffos';
+    }
+}
 ?>
